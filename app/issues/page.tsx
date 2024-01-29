be248e68d7f09ee91ee731e39236b4d90c417fd8
Josh <josh.uy25@gmail.com>
2024-01-30 01:15:16 +0800

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      IssuesPage
+      <Button>Add Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
